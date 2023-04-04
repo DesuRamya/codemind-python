b8a432df862f98b8b660b5867f96b2504cf45c57
DesuRamya <119840662+DesuRamya@users.noreply.github.com>
2023-04-04 09:44:21 +0530

    w=int(input()) if w==1:     print("Monday") elif w==2:     print("Tuesday") elif w==3:     print("Wednesday") elif w==4:     print("Thursday") elif w==5:     print("Friday") elif w==6:     print("Saturday") elif w==7:     print("Sunday") else:     print("Invlid input")

---
+++ Day_name__.py
@@ -0,0 +1,17 @@
+w=int(input())
+if w==1:
+    print("Monday")
+elif w==2:
+    print("Tuesday")
+elif w==3:
+    print("Wednesday")
+elif w==4:
+    print("Thursday")
+elif w==5:
+    print("Friday")
+elif w==6:
+    print("Saturday")
+elif w==7:
+    print("Sunday")
+else:
+    print("Invlid input")
