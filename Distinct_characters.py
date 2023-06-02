fff2e19d7590b51f39a2377f389cde3a08789314
DesuRamya <119840662+DesuRamya@users.noreply.github.com>
2023-06-02 08:07:43 +0530

    s=input() s1=str.lower(s) l={} for i in s1:     if i!=' ':         if i in l:             l[i]+=1         else:             l[i]=1 l1=[] for k,v in l.items():     if v==1:         l1.append(k) l1.sort() for i in l1:     print(i,end='')

--- Distinct_characters.py
+++ Distinct_characters.py
@@ -1,9 +1,16 @@
 s=input()
 s1=str.lower(s)
-l=[]
+l={}
 for i in s1:
-    if i not  in l and i!=' ':
-        l.append(i)
-l.sort()
-for i in l:
+    if i!=' ':
+        if i in l:
+            l[i]+=1
+        else:
+            l[i]=1
+l1=[]
+for k,v in l.items():
+    if v==1:
+        l1.append(k)
+l1.sort()
+for i in l1:
     print(i,end='')
