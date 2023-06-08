8d348ae135120dfc6b2f37d1ba4b07b6efc84bc1
DesuRamya <119840662+DesuRamya@users.noreply.github.com>
2023-06-08 20:57:23 +0530

    n=int(input()) l=n*2-1 for i in range(l):     for j in range(l):         if i<j:             m=i         else:             m=j         if m<l-i:             m=m         else:             m=l-i-1         if m<l-j-1:             m=m         else:             m=l-j-1         print(n-m,end=' ')     print()

---
+++ Printing_Pattern_using_Loops.py
@@ -0,0 +1,18 @@
+n=int(input())
+l=n*2-1
+for i in range(l):
+    for j in range(l):
+        if i<j:
+            m=i
+        else:
+            m=j
+        if m<l-i:
+            m=m
+        else:
+            m=l-i-1
+        if m<l-j-1:
+            m=m
+        else:
+            m=l-j-1
+        print(n-m,end=' ')
+    print()
